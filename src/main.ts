da5747189ba3a8fcab17b3239a9045e83987e93a
Александр Попов <alexand-90@yandex.ru>
2024-04-06 22:44:21 +0500

    syles and main

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,45 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import Handlebars from "handlebars";
+import * as Components from './components';
+import * as Pages from './pages';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const chatList = {
+    chatItem: [
+        {
+            name: "Иван",
+            message: "Привет, как дела?",
+            messageCount: 6,
+            time: '10:25'
+        },
+        {
+            name: "Иван",
+            message: "Привет, как дела?",
+            active: true,
+        },
+        {
+            name: "Иван",
+            message: "Привет, как дела?"
+        },
+        {
+            name: "Иван",
+            message: "Привет, как дела?"
+        },
+        {
+            name: "Иван",
+            message: "Привет, как дела?"
+        },
+    ]
+}
+
+
+Object.entries(Components).forEach(([name, comp]) => Handlebars.registerPartial(name, comp));
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = Handlebars.compile(Pages.ErrorPage)({ errNo: 500, message: 'Мы уже фиксим' });
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = Handlebars.compile(Pages.ErrorPage)({ errNo: 404, message: 'Не туда попали' });
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = Handlebars.compile(Pages.Registration)({});
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = Handlebars.compile(Pages.Login)({});
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = Handlebars.compile(Pages.Profile)({});
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = Handlebars.compile(Pages.EditProfile)({});
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = Handlebars.compile(Pages.EditPassword)({});
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = Handlebars.compile(Pages.ChatListPage)(chatList);
